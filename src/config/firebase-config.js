0dd483b56876820566ea69e2e6cf2be7ff9a8d8c
TsimakuridzeNikolozi <ntsim20@freeuni.edu.ge>
2024-05-21 20:27:28 +0400

    Initial setup

---
+++ src/config/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACA_c0Zcrj58m0CL44a7NwXLIt8UAPflM",
+  authDomain: "ol-academy-react-final-project.firebaseapp.com",
+  projectId: "ol-academy-react-final-project",
+  storageBucket: "ol-academy-react-final-project.appspot.com",
+  messagingSenderId: "663929369203",
+  appId: "1:663929369203:web:9e23a459893cd33f47b006",
+  measurementId: "G-50GP7SJPMY",
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);

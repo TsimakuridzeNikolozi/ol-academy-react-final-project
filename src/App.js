d100763159a922fa3498f0a1b64084244992acee
TsimakuridzeNikolozi <ntsim20@freeuni.edu.ge>
2024-05-22 03:01:52 +0400

    Added routes and grouped component exports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
-import { Authentication } from "./components/Authentication";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Item, Cart, OrderHistory } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <h1>Fragrances</h1>
-      <Authentication />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/item" element={<Item />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order-history" element={<OrderHistory />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

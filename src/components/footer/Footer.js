56c6fd400e9ee0e469bbcc67bfb5870b120362c0
TsimakuridzeNikolozi <ntsim20@freeuni.edu.ge>
2024-05-22 04:06:22 +0400

    Implemented Footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Col } from "reactstrap";
+
+const currentYear = new Date().getFullYear();
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Col
+      className="py-3 d-flex align-items-center justify-content-center"
+      style={{ backgroundColor: "#e0d0ff" }}
+    >
+      <span>&copy; {currentYear} All Rights Reserved</span>
+    </Col>
+  );
 };
 
 export default Footer;
